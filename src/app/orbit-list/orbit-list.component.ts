08ee0a921af17f9e1362ae0e3436912322b33903
Sushanth Krishnan <sushanth11@gmail.com>
2021-04-08 16:56:24 -0500

    i have no idea why 7 isn't working

--- src/app/orbit-list/orbit-list.component.ts
+++ src/app/orbit-list/orbit-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Directive } from '@angular/core';
+import { Component, OnInit, Input} from '@angular/core';
 import { Satelite } from '../sattelite';
 @Component({
   selector: 'app-orbit-list',
